9c5433437dc12e51b2ee8d14b15d0d19478334ce
Vina del Mar <vina@duoc.acad>
2025-05-19 12:48:16 -0400

    cambio de ruta de contenido a soporte

--- src/main/java/contenido/educativo/controller/SoporteController.java
+++ src/main/java/contenido/educativo/controller/SoporteController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-@RestController @RequestMapping("/api/v1/contenidos")
+@RestController @RequestMapping("/api/v1/soporte")
 public class SoporteController {
 
     @Autowired
